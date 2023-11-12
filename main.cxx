7f232e88ceccf65a64a11bcc903e29dba2e099d2
Atle Rystedt <at6051ry-s@student.lu.se>
2023-11-12 23:29:56 +0100

    year reading working

--- main.cxx
+++ main.cxx
@@ -3,21 +3,40 @@
 #include "csv.hpp"
 
 using namespace csv;
-
+int year;
+int month;
+int day;
+int hour;
+int temp;
+char grade;
 const std::string csvFilePath = "./datasets/Cleaned-Lund-data.csv";
+
 int main() {
   std::ifstream inputFile(csvFilePath);
-  
-  CSVReader reader(inputFile);
+  //std::vector<std::string> col_names = {"Date", "time", "Temp", "Grade"};
+CSVFormat format;
+//format.column_names(1, 2, 3, 4, 5);
+format.delimiter({'-'})
+      .no_header()
+      .quote(';');
+CSVReader reader(inputFile, format);
 
-double sum = 0;
  
 CSVRow row;
+
  
 for (auto& row: reader) {
-    // Note: Can also use index of column with [] operator
-    sum += row[0].get<double>();
-    std::cout << sum << std::endl;
+  
+    if (row[0].is_int()){
+      year = row[0].get<int>();
+      //std::cout << "date" << std::endl;
+    }
+    //kan markera detta som komentar month funkar inte, year gör dock
+    //if ( row[1].is_int()){
+      //month = row[1].get<int>();
+    //}
+    std::cout << year << month << std::endl;
+    
 }
   
   return 0;
